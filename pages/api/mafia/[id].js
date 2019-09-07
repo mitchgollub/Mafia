e15cb8ff08087dd45cb89b930ed16d61266b471b
Mitch Gollub <mgollub@live.com>
2019-09-07 16:52:11 -0400

    Added Name parameter

--- pages/api/mafia/[id].js
+++ pages/api/mafia/[id].js
@@ -2,6 +2,8 @@ const db = require('../../../lib/db');
 const escape = require('sql-template-strings')
 
 export default async (req, res) => {
+    console.log('id: ' + req.query.id);
+    console.log('name: ' + req.body.name);
     const resp = await db.query(escape`SELECT players FROM Games WHERE game_code = ${req.query.id}`)
 
     const players = JSON.parse(resp[0].players)
@@ -9,7 +11,7 @@ export default async (req, res) => {
     const index = Math.floor(Math.random() * (players.available.length))
     const selected = players.available[index]
     players.available.splice(index, 1);
-    players.current.push({id: selected.id, role: selected.role, name: "ME"});
+    players.current.push({id: selected.id, role: selected.role, name: req.body.name});
 
     const resp2 = await db.query(escape`UPDATE Games SET players=${JSON.stringify(players)} WHERE game_code = ${req.query.id}`)
 
--- pages/mafia.js
+++ pages/mafia.js
@@ -3,11 +3,21 @@ import MafiaPresentation from '../components/mafiaPresentation';
 
 export default class MafiaContainer extends Component {
     static getInitialProps = ({ query }) => {
-        return ({ id: query.id, character: '' });
+        return ({ id: query.id, name: query.name, character: '' });
     }
 
     async componentDidMount() {
-        const res = await fetch(`/api/mafia/${this.props.id}`);
+        const res = await fetch(`/api/mafia/${this.props.id}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: this.props.id,
+                name: this.props.name
+            })
+        });
         const state = await res.json()
         console.log(state)
         this.setState(state);
