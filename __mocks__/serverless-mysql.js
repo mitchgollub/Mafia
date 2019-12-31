ef0702acbbab70d716b6b21a02377304a9fe9907
mitchgollub <mgollub@live.com>
2019-12-31 17:03:38 -0500

    Finished Happy path on back-end

--- __mocks__/serverless-mysql.js
+++ __mocks__/serverless-mysql.js
@@ -2,19 +2,29 @@
 const mysql = jest.genMockFromModule('serverless-mysql');
 
 let mockDbResponses = [];
+let counter = 0;
+
+// Can use JEST Mocks to manage function state
+// https://jestjs.io/docs/en/es6-class-mocks
 function __setMockDbResonse(dbResponse) {
     mockDbResponses.push(dbResponse);
 }
 
+function __clearMockDbResponse() {
+    mockDbResponses = [];
+    counter = 0;
+}
+
 async function query(query) {
-    const mockDbResponse = mockDbResponses[0];
-    mockDbResponses.splice(0, 1);
+    const mockDbResponse = mockDbResponses[counter];
+    counter++; 
     return mockDbResponse;
 }
 
 mysql.mockImplementation(() => mysql);
 mysql.end = () => null;
 mysql.__setMockDbResonse = __setMockDbResonse;
+mysql.__clearMockDbResponse = __clearMockDbResponse;
 mysql.query = query;
 
 module.exports = mysql;
--- __tests__/api/mafia/[id].test.js
+++ __tests__/api/mafia/[id].test.js
@@ -1,5 +1,12 @@
 const mafia = require('../../../pages/api/mafia/[id]');
 const res = require('../../../__mocks__/res');
+const mockMySql = require('serverless-mysql');
+
+import roleDescriptions from '../../../configuration/roleDescriptions.json';
+
+beforeEach(() => {
+    mockMySql.__clearMockDbResponse();
+})
 
 test('Creates Player', async () => {
     const req = {
@@ -11,14 +18,47 @@ test('Creates Player', async () => {
             session: 'guid'
         }
     },
-        expected = {};
+        expected = {
+            id: "AAAA",
+            role: "Cop",
+            description: roleDescriptions['Cop'],
+        };
 
-    require('serverless-mysql').__setMockDbResonse([
+    mockMySql.__setMockDbResonse([
+        {
+            players: JSON.stringify({ current: [], available: [{ role: 'Cop', id: 1 }] })
+        }
+    ]);
+    mockMySql.__setMockDbResonse([]);
+
+    const actual = await mafia.default(req, res);
+
+    expect(actual.statusCode).toEqual(200);
+    expect(actual.json).toEqual(expected);
+});
+
+test('Returns \'Empty\' when no players available', async () => {
+    const req = {
+        query: {
+            id: 'AAAA'
+        },
+        body: {
+            name: 'Mitch',
+            session: 'guid'
+        }
+    },
+        expected = {
+            id: "AAAA",
+            name: "Mitch",
+            role: "Empty",
+            session: "guid",
+        };
+
+    mockMySql.__setMockDbResonse([
         {
             players: JSON.stringify({ current: [], available: [] })
         }
     ]);
-    require('serverless-mysql').__setMockDbResonse([]);
 
     const actual = await mafia.default(req, res);
 
